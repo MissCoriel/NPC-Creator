6021a6453e1ac83196e5a1c8ddd368a80da80ed5
Omega Terra Coriel <44127678+MissCoriel@users.noreply.github.com>
2021-04-12 12:57:26 -0500

    Add files via upload

---
+++ NPC Creator/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("NPC Creator")]
+[assembly: AssemblyDescription("Create NPCs easily with this simple application!\nProgrammer: Omega Terra Coriel")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Evil Pickle Games")]
+[assembly: AssemblyProduct("NPC Creator")]
+[assembly: AssemblyCopyright("Copyright © Evil Pickle 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("e2d51101-0481-43ff-8e77-1f0a722b43c5")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("2.0.1.0")]
+[assembly: AssemblyFileVersion("2.0.1.0")]
